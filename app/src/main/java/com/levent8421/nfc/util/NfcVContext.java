8f73dcac93dfbf7410f4a9795f2ba17eb32a7e7e
levent <levent8421@outlook.com>
2021-07-06 20:29:05 +0800

    NFC

---
+++ app/src/main/java/com/levent8421/nfc/util/NfcVContext.java
@@ -0,0 +1,11 @@
+package com.levent8421.nfc.util;
+
+import android.nfc.Tag;
+
+public class NfcVContext {
+    private final Tag tag;
+
+    public NfcVContext(Tag tag) {
+        this.tag = tag;
+    }
+}
